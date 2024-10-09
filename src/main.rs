3f33a0b7b823c85f6b6e1665ef1c771b488cc136
kazunori-sumi <ksumi1988@icloud.com>
2024-10-09 23:21:28 +0900

    接続成功

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+	println!("bind tcp listener!!!");
+	let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+	println!("TcpListener is bound to the localhost:8080");
+
+	for stream in listener.incoming() {
+		let stream = stream.unwrap();
+		println!("Connection established!");
+	}
 }
+
