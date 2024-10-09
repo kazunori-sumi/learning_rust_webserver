0a3d443996313cb6c974586be3d9928bdc41a53f
kazunori-sumi <ksumi1988@icloud.com>
2024-10-10 01:11:36 +0900

    serve html

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
-use std::net::TcpListener;
+use std::fs::File;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
-	println!("bind tcp listener!!!");
-	let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-	println!("TcpListener is bound to the localhost:8080");
+    println!("bind tcp listener!!!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("TcpListener is bound to the localhost:8080");
 
-	for stream in listener.incoming() {
-		let stream = stream.unwrap();
-		println!("Connection established!");
-	}
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
 }
 
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let mut file = File::open("hello.html").unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
